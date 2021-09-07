40b92dc0c3177015106f05d6b161491b15f15022
DEXXFP07 <frank.pilz.extern@jungheinrich.de>
2021-09-07 17:03:22 +0200

    einbau logik für play,eat, sleep,..

--- Tamagotchi.js
+++ Tamagotchi.js
@@ -11,18 +11,38 @@ class Tamagotchi {
     getname () {
         return this.name
     }
-    play () {
-        
+    play (playing) {
+        if(this.hunger > 5) {
+        this.hunger  += 2
+        this.mood    += 2
+        this.fatigue += 3
+        }
         return `${this.name} is playing`
     } 
-    eat  () {
+    eat  (eating) {
+        if(this.fatigue > 5){
+        this.hunger  -= 3
+        this.fatigue += 2 
+        } 
         return `${this.name} is eating`
     }
-    sleep() {
+    sleep(sleeping) {
+        if(this.hunger > 5) {
+        this.hunger  += 1
+        this.mood    += 1
+        this.fatigue += 0
+        }
+        else {
+            this.hunger  += 1
+            this.mood    -= 1
+            this.fatigue += 0
+        }
         return `${this.name} is sleeping`
     }
-    pet  () {
-        return `${this.name} is petting`
+    pet  (petting) {
+        this.hunger  += 1
+        this.mood    += 1
+        return `petting ${this.name}`
     } 
     getStatus() {
         if(this.hunger < 5) {
@@ -31,7 +51,7 @@ class Tamagotchi {
         if(this.fatigue < 5) {
             return "i am tired" 
          }
-        if(this.mood > 5) {
+        if(this.mood < 5) {
             return "i am happy" 
          }
          else {
@@ -42,9 +62,22 @@ class Tamagotchi {
 }    
 const my_tamagotchi = new Tamagotchi(name)
 
+// Ausgabe in Konsole
+// ===================
+console.log("Hallo ich bin " + my_tamagotchi.getname())
+console.log(my_tamagotchi.getStatus())
+console.log(my_tamagotchi.eat())
+console.log(my_tamagotchi.play())
+console.log(my_tamagotchi.sleep())
+console.log(my_tamagotchi.pet())
+console.log(my_tamagotchi)
+
+// Ausgabe auf Webseite
+// =====================
 alert("Hallo ich bin " + my_tamagotchi.getname())
 alert(my_tamagotchi.getStatus())
 alert(my_tamagotchi.eat())
 alert(my_tamagotchi.play())
 alert(my_tamagotchi.sleep())
 alert(my_tamagotchi.pet())
+alert(my_tamagotchi)
