2b36051b4ab2c5e4607a689a65f5a39c91a24ed4
DEXXFP07 <frank.pilz.extern@jungheinrich.de>
2021-09-07 16:15:58 +0200

    status ausgabe hinzugefügt

--- Tamagotchi.js
+++ Tamagotchi.js
@@ -2,9 +2,11 @@
 
 class Tamagotchi {
 
-    constructor(name, status) {
+    constructor(name, hunger, mood, fatigue) {
         this.name    = prompt("Wie soll Dein Tamagotchi heißen?");
-        this.status  = status
+        this.hunger  = hunger  = 0
+        this.mood    = mood    = 0
+        this.fatigue = fatigue = 0
     }
     getname () {
         return this.name
@@ -22,11 +24,26 @@ class Tamagotchi {
     pet  () {
         return `${this.name} is petting`
     } 
-    
+    getStatus() {
+        if(this.hunger < 5) {
+          return "i am hungry" 
+        }
+        if(this.fatigue < 5) {
+            return "i am tired" 
+         }
+        if(this.mood > 5) {
+            return "i am happy" 
+         }
+         else {
+            return " blahblahblah" 
+         }
+
+    }
 }    
 const my_tamagotchi = new Tamagotchi(name)
 
 alert("Hallo ich bin " + my_tamagotchi.getname())
+alert(my_tamagotchi.getStatus())
 alert(my_tamagotchi.eat())
 alert(my_tamagotchi.play())
 alert(my_tamagotchi.sleep())
