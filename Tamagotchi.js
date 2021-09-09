05ac5913255c8b0f98baf3ff211bc9e15c9b1436
DEXXFP07 <frank.pilz.extern@jungheinrich.de>
2021-09-09 15:36:28 +0200

    logiken angepasst

--- Tamagotchi.js
+++ Tamagotchi.js
@@ -16,6 +16,10 @@ class Tamagotchi {
         this.hunger  += 2
         this.mood    += 2
         this.fatigue += 3
+        }else {
+        this.hunger  += 1
+        this.mood    += 2
+        this.fatigue += 3
         }
         return `${this.name} is playing`
     } 
@@ -23,7 +27,10 @@ class Tamagotchi {
         if(this.fatigue > 5){
         this.hunger  -= 3
         this.fatigue += 2 
-        } 
+        } else {
+            this.hunger  -= 3
+            this.fatigue += 1 
+        }
         return `${this.name} is eating`
     }
     sleep(sleeping) {
@@ -35,7 +42,7 @@ class Tamagotchi {
         else {
             this.hunger  += 1
             this.mood    -= 1
-            this.fatigue += 0
+            this.fatigue -= 2
         }
         return `${this.name} is sleeping`
     }
@@ -45,81 +52,64 @@ class Tamagotchi {
         return `petting ${this.name}`
     } 
     getStatus() {
-        if(this.hunger < 5) {
+        if(this.hunger > 5) {
           return "i am hungry"
         }
-        if(this.fatigue < 5) {
+        if(this.fatigue > 5) {
             return "i am tired"
         }
-        if(this.mood < 5) {
-            return "i am happy"
+        if(this.mood > 5) {
+            return "i'm not happy"
         }
          else {
-            return " blahblahblah" 
+            return "I'm Happy" 
          }
 
     }
 }    
 const my_tamagotchi = new Tamagotchi(name)
 
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Eprobots JS Tutorial - Start");
-    function startstop_button_handler(){console.log("start stop button clicked");}
-    document.getElementById("btn_start_stop").addEventListener("click", startstop_button_handler);
+console.log("Hello i'm' " + my_tamagotchi.getname())
+      alert("Hallo i'm' " + my_tamagotchi.getname())
 
-    function play_button_handler(){console.log("play button clicked");}
+// Button Event listener
+// =======================
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Tamagotchi - Start");
+    
+    function status_button_handler() {
+        console.log(my_tamagotchi);
+        console.log(my_tamagotchi.getStatus())
+              alert(my_tamagotchi.getStatus())
+    }
+    document.getElementById("btn_status").addEventListener("click", status_button_handler);
+
+    function play_button_handler() {
+        my_tamagotchi.play(1)
+        console.log(my_tamagotchi.play())
+              alert(my_tamagotchi.play())
+    }
     document.getElementById("btn_play").addEventListener("click", play_button_handler);
 
-    function feed_button_handler(){console.log("feed button clicked");}
+    function feed_button_handler() {
+        my_tamagotchi.eat(1)
+        console.log(my_tamagotchi.eat())
+              alert(my_tamagotchi.eat())
+    }
     document.getElementById("btn_feed").addEventListener("click", feed_button_handler);
 
-    function sleep_button_handler(){console.log("sleep button clicked");}
+    function sleep_button_handler() {
+        my_tamagotchi.sleep(1)
+        console.log(my_tamagotchi.sleep())
+              alert(my_tamagotchi.sleep())
+    }
     document.getElementById("btn_sleep").addEventListener("click", sleep_button_handler);
 
-    function pet_button_handler(){console.log("pet button clicked");}
+    function pet_button_handler() {
+        my_tamagotchi.pet(1)
+        console.log(my_tamagotchi.pet())
+              alert(my_tamagotchi.pet())
+    }
     document.getElementById("btn_pet").addEventListener("click", pet_button_handler);});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Ausgabe in Konsole
-// ===================
-console.log("Hallo ich bin " + my_tamagotchi.getname())
-console.log(my_tamagotchi.getStatus())
-console.log(my_tamagotchi.eat())
-console.log(my_tamagotchi.play())
-console.log(my_tamagotchi.sleep())
-console.log(my_tamagotchi.pet())
-console.log(my_tamagotchi)
-
-// Ausgabe auf Webseite
-// =====================
-alert("Hallo ich bin " + my_tamagotchi.getname())
-alert(my_tamagotchi.getStatus())
-alert(my_tamagotchi.eat())
-alert(my_tamagotchi.play())
-alert(my_tamagotchi.sleep())
-alert(my_tamagotchi.pet())
-*/
-
-
-
 
 
